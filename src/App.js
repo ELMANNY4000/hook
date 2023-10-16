16d625d7aa525d8b01672df19ce47b6ac608bcde
Emmanuel <ulodoe@gmail.com>
2023-10-16 17:17:05 +0100

    post

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Effects from './components/effect/Effects'
+import Post from './components/Post/Post'
 
 const App = () => {
   return (
     <div>
       <Effects/>
+      <Post/>
     
     </div>
   )
